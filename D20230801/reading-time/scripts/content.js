c5e6fa4bd8cd51e65f3e71cfd0fd73cf42b2a5c7
sEGuoer <709074535@qq.com>
2023-08-01 16:07:57 +0800

    谷歌插件构造

---
+++ D20230801/reading-time/scripts/content.js
@@ -0,0 +1,13 @@
+const element = document.querySelector("h1")
+
+if (element !== null){
+    var para = document.createElement("span"); //创建新的<p> 元素
+    var node = document.createTextNode("这是一个新段落。"); //创建文本节点
+    para.appendChild(node); //向 <p> 元素追加这个文本节点
+    var para1 =document.createElement("img")
+    para1.src = "//www.baidu.com/img/flexible/logo/pc/result.png"
+    para.appendChild(para1)
+//向已有的元素追加这个新元素
+    element.appendChild(para);
+}
+
