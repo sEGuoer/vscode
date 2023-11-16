3d3c142cf23443f7f86320e160c4ce9381820b1c
sEGuoer <709074535@qq.com>
2023-11-16 16:29:24 +0800

    step 7 : 列表渲染(list Rendering)

--- seguoer-vue/src/App.vue
+++ seguoer-vue/src/App.vue
@@ -1,15 +1,36 @@
 <script setup>
 import {  ref } from 'vue'
 
-const text = ref('')
+// 给每个 todo 对象一个唯一的 id
+let id = 0
 
-function onInput(e) {
-  text.value = e.target.value
+const newTodo = ref('')
+const todos = ref([
+  { id: id++, text: 'Learn HTML' },
+  { id: id++, text: 'Learn JavaScript' },
+  { id: id++, text: 'Learn Vue' }
+])
+
+function addTodo() {
+  todos.value.push({ id: id++, text: newTodo.value })
+  newTodo.value = ''
+}
+
+function removeTodo(todo) {
+  todos.value = todos.value.filter((t) => t !== todo)
 }
 </script>
 
 <template>
-  <input :value="text" @input="onInput" placeholder="Type here">
-  <p>{{ text }}</p>
+  <form @submit.prevent="addTodo">
+    <input v-model="newTodo">
+    <button>Add Todo</button>
+  </form>
+  <ul>
+    <li v-for="todo in todos" :key="todo.id">
+      {{ todo.text }}
+      <button @click="removeTodo(todo)">X</button>
+    </li>
+  </ul>
 </template>
 
