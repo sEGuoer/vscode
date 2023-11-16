0be4e29612137fd721b9dd077b232f5a7805e9cc
sEGuoer <709074535@qq.com>
2023-11-16 16:24:52 +0800

    step 5 branch : use v-bind and v-on

--- seguoer-vue/src/App.vue
+++ seguoer-vue/src/App.vue
@@ -1,39 +1,15 @@
 <script setup>
-import { reactive, ref } from 'vue'
+import {  ref } from 'vue'
 
-const counter = reactive({ count: 0 })
-const message = ref('Hello World!')
-
-const titleClass = ref('title')
-const contentTitleClass = ref('content-title')
-const count = ref(0)
-
-function increment() {
-  count.value++
-}
 const text = ref('')
-const awesome = ref(true)
 
-function toggle() {
-  awesome.value = !awesome.value
+function onInput(e) {
+  text.value = e.target.value
 }
 </script>
 
 <template>
-  <h1 class="title">{{ message }}</h1>
-  <div><button class="count-title" @click="increment">count is: {{ count }}</button></div>
-  <input v-model="text" placeholder="Type here">
+  <input :value="text" @input="onInput" placeholder="Type here">
   <p>{{ text }}</p>
-  <button @click="toggle">toggle</button>
-  <h1 v-if="awesome">Vue is awesome!</h1>
-  <h1 v-else>Oh no 😢</h1>
 </template>
 
-<style>
-.title {
-  color: red;
-}
-.count-title{
-  color: blueviolet;
-}
-</style>
