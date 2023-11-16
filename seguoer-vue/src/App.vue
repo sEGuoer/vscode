476ff53ed10cd2c215f520f85dda17a9e4430dd2
sEGuoer <709074535@qq.com>
2023-11-16 16:12:29 +0800

    step 3 : Attribute 绑定(Attribute Bindings)

--- seguoer-vue/src/App.vue
+++ seguoer-vue/src/App.vue
@@ -3,9 +3,22 @@ import { reactive, ref } from 'vue'
 
 const counter = reactive({ count: 0 })
 const message = ref('Hello World!')
+
+const titleClass = ref('title')
+const contentTitleClass = ref('content-title')
+
 </script>
 
 <template>
-  <h1>{{ message }}</h1>
-  <p>Count is: {{ counter.count }}</p>
+  <h1 class="title">{{ message }}</h1>
+  <p class="count-title">Count is: {{ counter.count }}</p>
 </template>
+
+<style>
+.title {
+  color: red;
+}
+.count-title{
+  color: blueviolet;
+}
+</style>
