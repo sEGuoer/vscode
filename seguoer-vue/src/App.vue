61f4bcdf116f4c124d87d7ea7f0e579dd5c73605
sEGuoer <709074535@qq.com>
2023-11-17 11:21:50 +0800

    step 10 : 侦听器(Watchers)

--- seguoer-vue/src/App.vue
+++ seguoer-vue/src/App.vue
@@ -1,9 +1,25 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 
-const pElementRef = ref(null)
+const todoId = ref(1)
+const todoData = ref(null)
+
+async function fetchData() {
+  todoData.value = null
+  const res = await fetch(
+      `https://jsonplaceholder.typicode.com/todos/${todoId.value}`
+  )
+  todoData.value = await res.json()
+}
+
+fetchData()
+
+watch(todoId, fetchData)
 </script>
 
 <template>
-  <p ref="pElementRef">hello</p>
+  <p>Todo id: {{ todoId }}</p>
+  <button @click="todoId++">Fetch next todo</button>
+  <p v-if="!todoData">Loading...</p>
+  <pre v-else>{{ todoData }}</pre>
 </template>
